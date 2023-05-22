1cc35fd987f6193ec2c82bb5900ee82be1e66c47
flyjay123 <118870803+flyjay123@users.noreply.github.com>
2023-05-22 17:50:29 +0800

    修复图片对齐问题

--- Client/Chatting/chatwindow.cpp
+++ Client/Chatting/chatwindow.cpp
@@ -5,7 +5,7 @@
 #include <QImageReader>
 #include "stringtool.h"
 
-ChatWindow::ChatWindow(FriendInfo info,QWidget *parent) :
+ChatWindow::ChatWindow(const FriendInfo& info,QWidget *parent) :
     QWidget(parent),
     ui(new Ui::ChatWindow),
     m_info(info)
@@ -19,20 +19,9 @@ ChatWindow::ChatWindow(FriendInfo info,QWidget *parent) :
     QFont defaultFont = ui->textEdit->font();
     defaultFont.setPointSize(13); // 设置字体大小，根据您的需要调整大小
     ui->textEdit->setFont(defaultFont);
-
-
-
-    //ui->textEdit->setPlainText(QString("System :  you are chatting with %1\n").arg(m_info->name));
-//    sendMessage("hello");
-//    sendMessage("hello world");
-//    sendImage("C:\\Users\\k\\Documents\\GitHub\\OurChat\\Client\\src\\QQIcon\\chatgpt-logo.png");
-//    sendMessage("hello");
-//    sendImage("C:\\Users\\k\\Documents\\GitHub\\OurChat\\Client\\src\\QQIcon\\icon15.jpg");
-//    sendMessage("hello world");
-//    sendFile("a.pdf","C:\\pdf\\a.pdf");
 }
 
-ChatWindow::ChatWindow(GroupInfo info, QWidget *parent):
+ChatWindow::ChatWindow(const GroupInfo& info, QWidget *parent):
         QWidget(parent),
         ui(new Ui::ChatWindow)
 {
@@ -50,7 +39,7 @@ ChatWindow::~ChatWindow()
 }
 
 //flag:0 self 1 other
-void ChatWindow::pushMsg(QString msg, int flag)
+void ChatWindow::pushMsg(const QString& msg, int flag)
 {
     QTextCursor cursor = ui->textEdit->textCursor();
     cursor.movePosition(QTextCursor::End);
@@ -139,59 +128,7 @@ void ChatWindow::sendFile(const QString &fileName, const QString &filePath,int f
 
 }
 
-void ChatWindow::sendImage(const QImage &image, int flag) {
-    if(image.isNull())
-    {
-        textBrowser->insertPlainText("Broken image!");
-    }
-    //设置textBrowser光标到最后
-    QTextCursor cursor = textBrowser->textCursor();
-    cursor.movePosition(QTextCursor::End);
-    textBrowser->setTextCursor(cursor);
-    int maxWidth = 200;
-    int maxHeight = 200;
-
-    // 计算适当的宽度和高度，以保持图片的纵横比
-    int newWidth = qMin(image.width(), maxWidth);
-    int newHeight = newWidth * image.height() / image.width();
-
-    if (newHeight > maxHeight) {
-        newHeight = maxHeight;
-        newWidth = newHeight * image.width() / image.height();
-    }
-
-    // 缩放图像
-    QImage scaledImage = image.scaled(newWidth, newHeight, Qt::KeepAspectRatio, Qt::SmoothTransformation);
-
-    // 将 QImage 转换为 QPixmap
-    QPixmap pixmap = QPixmap::fromImage(scaledImage);
-
-    // 创建一个 QTextImageFormat 对象
-    QTextImageFormat imageFormat;
-
-    // 将 QPixmap 转换为 Base64 编码的二进制数据
-    QByteArray byteArray;
-    QBuffer buffer(&byteArray);
-    buffer.open(QIODevice::WriteOnly);
-    pixmap.save(&buffer, "PNG");
-    buffer.close();
-
-    // 将 Base64 编码的二进制数据转换为 QString
-    QString base64Image = QString::fromLatin1(byteArray.toBase64().data());
-
-    // 设置 QTextImageFormat 属性
-    imageFormat.setName("data:image/png;base64," + base64Image);
-
-    // 你也可以将图片插入到 QTextEdit 作为 HTML（可选）
-    ui->textEdit->insertHtml("<img src='data:image/png;base64," + base64Image + "' />");
-    ui->textEdit->insertHtml("<br>"); // 添加一个换行符
-}
-
 void ChatWindow::sendImages(const QList<QString>& base64Images, int flag) {
-    // 设置textBrowser光标到最后
-    QTextCursor cursor = textBrowser->textCursor();
-    cursor.movePosition(QTextCursor::End);
-    textBrowser->setTextCursor(cursor);
     for (const QString& base64Image : base64Images) {
         QByteArray imageData = QByteArray::fromBase64(base64Image.toLatin1());
         QImage image;
@@ -208,13 +145,24 @@ void ChatWindow::sendImages(const QList<QString>& base64Images, int flag) {
         imageFormat.setName("data:image/png;base64," + base64Image);
         imageFormat.setProperty(QTextFormat::UserProperty, originalBase64Image);
 
+        // 创建新的 QTextCursor
+        QTextCursor cursor = textBrowser->textCursor();
+        cursor.movePosition(QTextCursor::End);
         cursor.insertImage(imageFormat);
+
+        // 插入换行
+        pushMsg("",flag);
+
+        // 设置文本对齐方式
+        QTextBlockFormat blockFormat;
+        blockFormat.setAlignment(flag ? Qt::AlignLeft : Qt::AlignRight);
+        cursor.setBlockFormat(blockFormat);
     }
 
-    textBrowser->insertHtml("<br>");
     textBrowser->verticalScrollBar()->setValue(textBrowser->verticalScrollBar()->maximum()); // 自动滚动到底部
     qDebug() << "send images";
 }
+
 
 void ChatWindow::sendContentFromInput(const QString& htmlContent, int flag) {
     // 设置textBrowser光标到最后
@@ -224,9 +172,9 @@ void ChatWindow::sendContentFromInput(const QString& htmlContent, int flag) {
 
     QList<QPair<QString, QImage>> contents = StringTool::extractContent(htmlContent);
 
-    for (const auto &content : contents) {
+    for (const auto& content : contents) {
         if (!content.first.isEmpty()) {
-            // 发送文本消息
+            // 发送文字消息
             sendMessage(content.first, flag);
         }
 
@@ -245,6 +193,8 @@ void ChatWindow::sendContentFromInput(const QString& htmlContent, int flag) {
     textBrowser->verticalScrollBar()->setValue(textBrowser->verticalScrollBar()->maximum()); // 自动滚动到底部
     qDebug() << "send content from input";
 }
+
+
 
 
 
